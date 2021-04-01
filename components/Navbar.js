e1572d7d1eab99a5b068bb17a56279d6f82adcce
Ola <oladabro@gmail.com>
2021-04-01 21:44:09 +0200

    add links between pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,26 @@
+import Link from 'next/link';
+
 const Navbar = () => {
   return (
     <nav>
       <div className='logo'>
         <h3>Szkolne koło harcerskie</h3>
       </div>
-      <a>Strona Główna</a>
-      <a>O Nas</a>
-      <a>Nasza misja</a>
-      <a>Drużyna</a>
-      <a>Kontakt</a>
+      <Link href='/'>
+        <a>Strona Główna</a>
+      </Link>
+      <Link href='/about'>
+        <a>O Nas</a>
+      </Link>
+      <Link href='/mission'>
+        <a>Nasza misja</a>
+      </Link>
+      <Link href='/team'>
+        <a>Drużyna</a>
+      </Link>
+      <Link href='/contact'>
+        <a>Kontakt</a>
+      </Link>
     </nav>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
@@ -17,6 +18,9 @@ export default function Home() {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </p>
+      <Link href='/team'>
+        <a>Poznaj naszą drużynę:)</a>
+      </Link>
       <Footer />
     </div>
   );
