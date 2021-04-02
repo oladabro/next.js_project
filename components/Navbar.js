b67bd15106cb838df7c5a49eb0291a944bfa0e5e
Ola <oladabro@gmail.com>
2021-04-02 17:45:04 +0200

    change image to Image compontent in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <nav>
       <div className='logo'>
-        <img src='/next-js-seeklogo.com.svg' width='100' />
+        {/* <img src='/next-js-seeklogo.com.svg' width='100' /> */}
+        <Image src='/next-js-seeklogo.com.svg' width='100' height='60' />
       </div>
       <Link href='/'>
         <a>Strona Główna</a>
