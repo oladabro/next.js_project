0e5ea79731ba074a7fc24adb7913f1c35c6ab0f8
Ola <oladabro@gmail.com>
2021-04-03 20:36:51 +0200

    add getStaticPaths to [id].js

--- pages/team/[id].js
+++ pages/team/[id].js
@@ -1,3 +1,19 @@
+export const getStaticPaths = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  const paths = data.map((scout) => {
+    return {
+      path: { id: scout.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
   return (
     <div>
