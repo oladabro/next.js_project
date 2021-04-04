53a299ad5e3619ef396e57606ba794ad1953ee3c
Ola <oladabro@gmail.com>
2021-04-04 22:46:16 +0200

    add content to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -9,7 +9,12 @@ const Contact = () => {
       </Head>
       <div>
         <h3>Chcesz do nasz dołączyć?</h3>
-        <p>Napisz do nas</p>
+        <p>Napisz do nas na adres: harcerze12@harcerze.org</p>
+        <p>Zadzwoń: 123-234-567</p>
+        <p>
+          Lub przyjdź na jedno z naszych spotkań, które odbywają się w każdą
+          sobotę o godz. 9.00 w ZSP12 w Żorach
+        </p>
       </div>
     </>
   );
