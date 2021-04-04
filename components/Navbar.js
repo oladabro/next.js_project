8eef5e6b5c6587035898930a595bcbe4414cbb8d
Ola <oladabro@gmail.com>
2021-04-04 22:41:07 +0200

    add style to Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,19 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+// import styles from '../styles/Navbar.module.css';
+
 const Navbar = () => {
+  const handleClick = (e) => {
+    const links = document.querySelectorAll('a');
+
+    links.forEach((el) => {
+      el.classList.remove('linkSelected');
+    });
+
+    e.target.classList.toggle('linkSelected');
+  };
+
   return (
     <nav>
       <div className='logo'>
@@ -9,20 +21,31 @@ const Navbar = () => {
         <Image src='/next-js-seeklogo.com.svg' width='70' height='42' />
         <h3>Szkolne Koło Harcerskie</h3>
       </div>
+
       <Link href='/'>
-        <a>Strona Główna</a>
+        <a onClick={(e) => handleClick(e)} className='link'>
+          Strona Główna
+        </a>
       </Link>
       <Link href='/about'>
-        <a>O Nas</a>
+        <a onClick={(e) => handleClick(e)} className='link'>
+          O Nas
+        </a>
       </Link>
       <Link href='/mission'>
-        <a>Nasza misja</a>
+        <a onClick={(e) => handleClick(e)} className='link'>
+          Nasza misja
+        </a>
       </Link>
       <Link href='/team'>
-        <a>Drużyna</a>
+        <a onClick={(e) => handleClick(e)} className='link'>
+          Drużyna
+        </a>
       </Link>
       <Link href='/contact'>
-        <a>Kontakt</a>
+        <a onClick={(e) => handleClick(e)} className='link'>
+          Kontakt
+        </a>
       </Link>
     </nav>
   );
